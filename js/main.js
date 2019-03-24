07750ece2a847ace76600af8ad64569bd3c511b0
juvelist <39278128+juvelist@users.noreply.github.com>
2019-03-24 11:28:04 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,40 @@
 // typewriter
 
-$(".header__main_text-question").typewriter({
-    text: "Yup, we can du that.",
-    waitingTime: 1000,
-    delay: 100,
-    cursor: true,
+// $(".header__main_text-question").typewriter({
+//     text: "Yup, we can du that.",
+//     waitingTime: 1000,
+//     delay: 100,
+//     cursor: true,
+// });
+
+//owl-carousel
+$("#slider-banner").owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        }
+    },
+    autoplay:true,
+    autoplayTimeout:6000,
+    autoplaySpeed:3000,
+    autoplayHoverPause:true
+});
+
+$("#slider-block").owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText:false,
+    responsive:{
+        0:{
+            items:1
+        }
+    },
+    autoplay:true,
+    autoplayTimeout:6000,
+    autoplaySpeed:3000,
+    autoplayHoverPause:true
 });
